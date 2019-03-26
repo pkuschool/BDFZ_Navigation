fb03d7b5478fd22e083d6838d354849e5831f519
YC L <30719670+smallg0at@users.noreply.github.com>
2019-03-26 12:53:14 +0800

    add clock

--- js/go.js
+++ js/go.js
@@ -1,4 +1,49 @@
 function GoTo(to_url){
     window.open(to_url,'_blank');
 }
-var timebar = document.getElementById('')
+window.setInterval(uptime, 1000);
+ function uptime() {
+    var thedate = new Date;
+    var timelbl=document.getElementById("time-desktop");
+    var datelbl=document.getElementById("date-desktop");
+    var datestring = "";
+     switch (thedate.getDay()) {
+        case 1:
+            datestring = '一'
+            break;
+         case 2:
+             datestring = '二'
+             break;
+         case 3:
+             datestring = '三'
+             break;
+         case 4:
+             datestring = '四'
+             break;
+         case 5:
+             datestring = '五'
+             break;
+         case 6:
+             datestring = '六'
+             break;
+         case 7:
+             datestring = '日'
+             break;
+        default:
+            break;
+    }
+    var minstring = "";
+    var secstring = "";
+    if(thedate.getMinutes()<10){
+        minstring = '0'+thedate.getMinutes().toString();
+    }else{
+        minstring = thedate.getMinutes().toString();
+    }
+     if (thedate.getSeconds() < 10) {
+         secstring = '0' + thedate.getSeconds().toString();
+     }else{
+         secstring = thedate.getSeconds().toString();
+     }
+    timelbl.innerHTML= thedate.getHours() + ':' + minstring + ":" +secstring;
+    datelbl.innerHTML= thedate.getMonth() + '月' + thedate.getDay() + '日，星期' + datestring;
+ }
