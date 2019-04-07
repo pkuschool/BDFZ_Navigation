c957974ab9994557d35af23dabe8c63912436e00
SubITServer <subit@i.pkuschool.edu.cn>
2019-04-07 08:31:27 +0800

    login style enhancement

--- login.php
+++ login.php
@@ -1,45 +1,46 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <?php
-    //phpinfo();
+<?php
+    $e=1;
     $user=$_POST["user"];
     $pwd=$_POST["pwd"];
+    $msg = "";
     include_once "dbpwd.php";
     $conn=mysqli_connect("114.115.146.81:3306","root",$dbpwd);
     mysqli_query($conn,"use userinfo");
     $sql="select * from info where user='$user'";
     $res=mysqli_query($conn,$sql);
-    mysqli_close($conn);
-?>
-    <meta http-equiv="refresh" content='2;url=./index.php?username=<?php echo "$user";?>' />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>正在登录...</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="./css/materialize.css">
-    <script src="./js/materialize.js"></script>
-</head>
-<body>
-    <div class="container">
-        <p class="centered">
-    
-    <?php
         if(mysqli_num_rows($res)){
             $row=mysqli_fetch_row($res);
             if($pwd!=$row[2]){
-                echo "用户不存在或密码有误";
-                exit;
+                $msg="用户不存在或密码有误";
+                $e=0;
             }
         }
         else{
-            echo "用户不存在或密码有误";
-            exit;
+            $msg="用户不存在或密码有误";
+            $e=0;
         }
-    echo "登录成功！<br />";
-    echo $user; 
-
-    ?></p></div>
+    if($e==1)$msg= "欢迎 $user 登录!";
+    mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="refresh" content='1;url=./index.php' />-->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/jquery.form.js"></script>
+    <link rel="stylesheet" href="./css/materialize.css">
+</head>
+<body class="blue-grey">
+    <div class="container center center-align center-block centered">
+        <h1 class="white-text" style="margin-top: 150px; text-shadow: 1px 1px light-grey"><?php echo $msg;?></h1>
+    </div>
+    <script>
+        $.post("./index.php",(username:<?php if($e==1)echo $user;?>),function(data,textStatus){});
+        
+    </script>
+    
 </body>
 </html>
+  
