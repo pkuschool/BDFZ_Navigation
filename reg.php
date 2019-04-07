81dc07c3e40775ff29f661b8e6e0cf06846e9097
SubITServer <subit@i.pkuschool.edu.cn>
2019-04-07 13:18:22 +0800

    bug

--- reg.php
+++ reg.php
@@ -1,10 +1,12 @@
+<!doctype html>
+<html>
 <?php
 //phpinfo();
 $e=1;
-$user=$_GET["user"];
-$pwd=$_GET["pwd"];
-$li=$_GET["li"];
-$website=$_GET["website"];
+$user=$_POST["user"];
+$pwd=$_POST["pwd"];
+$li=$_POST["li"];
+$website=$_POST["website"];
 if($user==null){
     echo "用户名不能为空";
     $e=0;
@@ -19,22 +21,34 @@ mysqli_query($conn,"use userinfo");
 $sql="select * from info where user='$user'";
 $res=mysqli_query($conn,$sql);
 if(mysqli_num_rows($res)){
-    echo "用户名已存在";
+    $msg="用户名已存在";
     $e=0;
 }
 $sql="insert into info(user,pwd,li,website) values('$user','$pwd','$li','$website')";
 mysqli_query($conn,$sql);
-if($e==1)echo "注册成功";
+if($e==1) $msg="注册成功";
 mysqli_close($conn);
 ?>
-<!doctype html>
-<html>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="refresh" content='1;url=./index.php' />-->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/jquery.form.js"></script>
+    <link rel="stylesheet" href="./css/materialize.css">
+</head>
+<body class="blue-grey">
+    <div class="container center center-align center-block centered">
+        <h1 class="white-text" style="margin-top: 150px; text-shadow: 1px 1px light-grey"><?php echo $msg;?></h1>
+    </div>
     <form method="POST" style="display:none" action="index.php" id="f1" enctype="multipart/form-data">
         <input name="username" style="display:none" type="text" value="<?php echo $user;?>"/>
     </form> 
     <script>
-        setTimeout("$('#f1').submit()",2000);
+        <?php
+        if($e==0) echo "window.location.href='reg.html'";
+        else echo "setTimeout('$('#f1').submit()',2000)";
+        ?>
+       ;
     </script>
 </body>
-    </html>
+</html>
